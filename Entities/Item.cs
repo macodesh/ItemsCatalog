1527dd63db89d63ffd3e3f2164d579f0260f3fc4
Marlon Couto <devmarloncouto@gmail.com>
2023-09-11 14:14:37 -0300

    Atualiza Entities

--- Entities/Item.cs
+++ Entities/Item.cs
@@ -2,7 +2,7 @@ namespace Catalog.Entities;
 public record Item
 {
     public Guid Id { get; init; }
-    public string Name { get; set; } = "";
+    public string? Name { get; set; }
     public decimal Price { get; init; }
     public DateTimeOffset CreatedDate { get; init; }
 }
