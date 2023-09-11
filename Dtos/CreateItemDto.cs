2f7e7fe96a82c812a5671ab576c5238b722ef693
Marlon Couto <devmarloncouto@gmail.com>
2023-09-11 14:14:50 -0300

    Atualiza Dtos

---
+++ Dtos/CreateItemDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Catalog.Dtos;
+public record CreateItemDto
+{
+    [Required]
+    public string? Name { get; set; }
+
+    [Required]
+    [Range(1, 1000)]
+    public decimal Price { get; set; }
+}
