05496aa86dd0804db1ceb2cd62f76f3ee01b107b
Marlon Couto <devmarloncouto@gmail.com>
2023-09-11 14:13:15 -0300

    Cria MongoDbSettings.cs

---
+++ Settings/MongoDbSettings.cs
@@ -0,0 +1,12 @@
+namespace Catalog.Settings;
+public class MongoDbSettings
+{
+    public string? Host { get; set; }
+    public int Port { get; set; }
+    public string? User { get; set; }
+    public string? Password { get; set; }
+    public string ConnectionString
+    {
+        get => $"mongodb://{User}:{Password}@{Host}:{Port}";
+    }
+}
